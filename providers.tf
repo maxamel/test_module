e20b182adfc69dcfe169fbf6c8810da28113b33b
Max Amelchenko <maxamel2002@gmail.com>
2025-03-27 11:46:30 +0200

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  alias      = "plain_text_access_keys_provider"
+  region     = "us-west-1"
+  access_key = "AKIAIOSFODNN7EXAMPLE"
+  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+}
