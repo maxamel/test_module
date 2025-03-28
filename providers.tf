1ec1479cdb3b50bb59e9eb3eb715578999e2c2cd
Max Amelchenko <maxamel2002@gmail.com>
2025-03-28 21:09:22 +0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -4,3 +4,9 @@ provider "aws" {
   access_key = "AKIAIOSFODNN7EXAMPLE"
   secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 }
+
+provider "aws" {
+  region     = "us-west-1"
+  access_key = "AKIAIOSFODNN7EXAMPLE"
+  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+}
